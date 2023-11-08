dfe1a50ef7b55712160c427dff1a93d6fa557aa0
Theagns <markus.agnsgaard@hotmail.com>
2023-11-08 10:37:06 +0100

    done

--- index.test.js
+++ index.test.js
@@ -1,24 +1,25 @@
-const request = require("supertest");
-const app = require("./index.js");
+// const request = require("supertest");
+// const app = require("./index.js");
 
-describe("GET /", () => {
-  it("responds with JSON message", (done) => {
-    request(app)
-      .get("/")
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .expect({ response: "Hello From Thetips4you" })
-      .end(done);
-  });
-});
-
-// describe("GET /will", () => {
+// describe("GET /", () => {
 //   it("responds with JSON message", (done) => {
 //     request(app)
-//       .get("/will")
+//       .get("/")
 //       .expect("Content-Type", /json/)
 //       .expect(200)
-//       .expect({ response: "Hello World" })
+//       .expect({ response: "Hello From Thetips4you" })
 //       .end(done);
 //   });
 // });
+
+// // describe("GET /will", () => {
+// //   it("responds with JSON message", (done) => {
+// //     request(app)
+// //       .get("/will")
+// //       .expect("Content-Type", /json/)
+// //       .expect(200)
+// //       .expect({ response: "Hello World" })
+// //       .end(done);
+// //   });
+// // });
+// //
