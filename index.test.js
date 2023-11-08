46ccc25329de4bad0fe21487d127923ba7299b6f
Theagns <markus.agnsgaard@hotmail.com>
2023-11-08 09:58:19 +0100

    Test

---
+++ index.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const app = require("./index.js");
+
+describe("GET /", () => {
+  it("responds with JSON message", (done) => {
+    request(app)
+      .get("/")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect({ response: "Hello From Thetips4you" })
+      .end(done);
+  });
+});
+
+const request = require("supertest");
+const app = require("./index.js");
+
+describe("GET /will", () => {
+  it("responds with JSON message", (done) => {
+    request(app)
+      .get("/will")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect({ response: "Hello World" })
+      .end(done);
+  });
+});
